04b765ae489ceb786d6e6f41caf3c23063c1bca6
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-02-25 14:39:37 -0500

    add home, about, contact + navigation with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import About from './About';
 import Contact from './Contact';
 import Home from './Home';
+import NavBar from './NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
-        <About />
-        <Contact />
+        <Router>
+          <Fragment>
+            <NavBar />
+            <Switch>
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </Fragment>
+        </Router>
       </div>
     )
   }
