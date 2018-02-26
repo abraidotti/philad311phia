c86d2e1851eff1afaa9f1558cc89313be3377bae
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-02-26 14:15:37 -0500

    fix up about and readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Really Philly
 
+- Search through Philadelphia's 311 records and see entries with images attached.
+
+```https://github.com/abraidotti/philad311phia.git
+cd philad311phia
+npm install
+npm start
+```
+
 ## Todo
 
 - switch CSS to Sass
--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,12 @@ class About extends Component {
   render (){
     return (
       <div>
-        <p>Let's see what Philly's 311 service has been up to by looking at the service entries with images attached.</p>
-        <p>Made with React, React Router, and Philly's <a href="https://www.opendataphilly.org/dataset/311-service-and-information-requests">311 API</a>👍🏼.</p>
-        <p>Styled with <a href="https://milligram.io">Milligram</a>.</p>
+        <p>Let's see what Philly's 311 service has been up to by looking at all the entries with images attached.</p>
+        <ul>
+        <li>Made with React, React Router, and Philly's <a href="https://www.opendataphilly.org/dataset/311-service-and-information-requests">311 API</a>👍🏼.</li>
+        <li>Styled with <a href="https://milligram.io">Milligram</a>.</li>
+        <li>Inspired by a pile of trash on my street that persists to this day...</li>
+      </ul>
       </div>
     );
   }
